a27c8d8acfe2bfdfa92195498131d57cafdef587
Tirth S Pandya <teamfznltd@gmail.com>
2026-07-12 13:44:48 +0530

    ..

--- Week-02/01-async-js/easy/2-counter.js
+++ Week-02/01-async-js/easy/2-counter.js
@@ -3,3 +3,14 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+
+const counter = (function count(n){
+    return function(){
+        console.log(n++);
+        setTimeout(counter, 1000);
+    }
+})(0); // Start counting from 0
+
+// Start the counter
+counter();
+
