9e1726bfea026c877f40489d0533ffc4f2c36a94
Tirth S Pandya <teamfznltd@gmail.com>
2026-08-11 14:53:55 +0530

    solve some ques

--- Week-02/01-async-js/medium/1-file-cleaner.js
+++ Week-02/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,27 @@
 // ```
 // hello world my name is raman
 // ```
+
+import fs from 'fs';
+
+function fileCleaner(filepath, format){
+    fs.writeFile(filepath, format, (err, res) => {
+        if(err){
+            console.log(`Error in appending file ${err}`);
+        } else {
+            console.log(`Contents appended in file ${res}`);
+            let removedSpaces = format.replace(/\s+/g, ' ').trim();
+            fs.appendFile(filepath, removedSpaces, (err, res) => {
+                if(err){
+                    console.log(`Error in appending file ${err}`);
+                } else {
+                    console.log(`Contents appended in file ${res}`);
+                }
+            })
+        }
+    })
+}
+
+let sampleData = 'hi   there\n'
+
+fileCleaner('./test.txt', sampleData);
